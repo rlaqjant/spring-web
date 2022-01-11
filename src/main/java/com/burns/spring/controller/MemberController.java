57dfbd0935fc706084baf059e4acc66b5f9d2482
rlaqjant <n123001@gmail.com>
2022-01-11 21:14:23 +0900

    기능 기본

--- src/main/java/com/burns/spring/controller/MemberController.java
+++ src/main/java/com/burns/spring/controller/MemberController.java
@@ -1,8 +1,15 @@
 package com.burns.spring.controller;
 
+import com.burns.spring.domain.Member;
+import com.burns.spring.form.MemberForm;
 import com.burns.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController
@@ -13,5 +20,31 @@ public class MemberController
     public MemberController(MemberService memberService)
     {
         this.memberService = memberService;
+    }
+
+
+    @GetMapping("/members/new")
+    public String createForm()
+    {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form)
+    {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model)
+    {
+        List<Member> memberList = memberService.findMembers();
+        model.addAttribute("members", memberList);
+        return "members/memberList";
     }
 }
