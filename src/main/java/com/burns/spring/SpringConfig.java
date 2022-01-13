1a5b1bfdc68523dbc7993859865549ea0907fcb6
rlaqjant <n123001@gmail.com>
2022-01-13 21:55:59 +0900

    JDBC

--- src/main/java/com/burns/spring/SpringConfig.java
+++ src/main/java/com/burns/spring/SpringConfig.java
@@ -1,14 +1,25 @@
 package com.burns.spring;
 
+import com.burns.spring.repository.JDBCMemberRepository;
 import com.burns.spring.repository.MemberRepository;
 import com.burns.spring.repository.MemoryMemberRepository;
 import com.burns.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig
 {
+    DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService()
@@ -19,7 +30,8 @@ public class SpringConfig
     @Bean
     public MemberRepository memberRepository()
     {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JDBCMemberRepository(dataSource);
     }
 
 }
